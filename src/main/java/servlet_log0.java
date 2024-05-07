766bddd6756bbc7de114595866b3678b7d37d64a
ppauetii <128194514+ppauetii@users.noreply.github.com>
2024-05-07 15:33:07 +0200

    servlet_log0.java comentado
    comentado todo, he dejado un método comentado por si acaso, porque no se llama a la cabecera en el html, y puede que haya que hacerlo, pero por si acaso se me ha escapado lo he dejado comentado

--- src/main/java/servlet_log0.java
+++ src/main/java/servlet_log0.java
@@ -13,27 +13,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+//creamos la clase que hereda de HttpServlet, de esta manera puede resolver peticiones HTTP
 public class servlet_log0 extends HttpServlet {
-	
+
+	//identificador para la serialización
 	private static final long serialVersionUID = 1L;
+
+	//creamos la cabecera aparte
 	String preTituloHTML5 = "<!DOCTYPE html>\n<html>\n<head>\n<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />";
 
+    //constructor de la clase, que hereda directamente de la superclase
     public servlet_log0() {
         super();
     }
-
+	//esto es lo que ejecuta cuando recibe petición GET
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//responde a la petición con "Served at:" seguido de la ruta de la solicitud
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
+	//esto es lo que se ejecuta cuando recibe petición POST
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+		//creamos un Writer de respuesta al cliente al que llamamos "out"
 		PrintWriter out = response.getWriter();
+		//establecemos el tipo de contenido como html
 		response.setContentType("text/html");
-		
+
+		//creamos el string del resultado
+		//contiene dirección de host remoto, fecha y hora actual, método de solicitud (será POST porque está en doPost) y la URL de la solicitud
 		String result = request.getRemoteHost() + " - " +LocalDateTime.now() + " - " + request.getMethod() + " " + request.getRequestURL();
-		
+
+		//Lo dejo comentado porque no se si se ha llegado a llamar
+		//out.println(preTituloHTML5)
+		//creamos el documento HTML en el que mostramos el resultado
 		out.println("<html><head><title>Log 0</title></head><body>");
 		out.println("<span>" + result + "</span>");
 		out.println("</body></html>");
