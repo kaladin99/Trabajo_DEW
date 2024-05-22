56003184bb8df3f639877b712432f64688b57eb2
Joel Revert <jrevertvila@gmail.com>
2024-05-22 23:16:00 +0200

    limpiando codigo login

--- src/main/java/login.java
+++ src/main/java/login.java
@@ -17,6 +17,7 @@ import org.json.*;
  */
 public class login extends HttpServlet {
 	
+	/* La finalidad de esta clase es unicamente para la realización del HashMap para la lista de usuarios del centro educativo */
 	class UserCE {
 		private String username;
 		private String password;
@@ -28,7 +29,8 @@ public class login extends HttpServlet {
 	    public String getUsername() { return this.username; }
 	    public String getPassword() { return this.password; }
 	}
-	
+	 
+	/* Este hashmap contiene todos los usuarios disponibles del centro eductativo y su relacion de credenciales entre tomcat y centro educativo */
 	Map<String, UserCE> users = new HashMap<String, UserCE>() {{
         put("12345678W", new UserCE("12345678W", "123456"));
 
@@ -36,10 +38,6 @@ public class login extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	private String urlCE = "http://localhost:9090/CentroEducativo";
-	private static final String preHTML5 = "<!DOCTYPE html>\n<html lang=\"es-es\">\n" +
-			"<head>\n<meta charset=\"utf-8\" />\n" +
-			"<title>Login?</title>\n</head>\n<body>\n" +
-			"<h1>Login?</h1>\n";
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -64,33 +62,33 @@ public class login extends HttpServlet {
 
 		response.setContentType("text/html");
 		response.setCharacterEncoding("UTF-8"); 
-		PrintWriter out = response.getWriter();
-		out.println(preHTML5);
+		
+		/* Obtenemos la sesion actual, si no existe la crea*/
 		HttpSession sesion = request.getSession();
-		String tomcatUser = request.getRemoteUser();
-		if(sesion.getAttribute("key") == null) {
+		String tomcatUser = request.getRemoteUser(); //Nombre de usuario autenticado mediante tomcat (El alert user/password)
+		if(sesion.getAttribute("key") == null) { //En caso de que la sesión que hemos obtenido no tenga key, significa que no la habiamos creado nosotros y es nueva
 			if (tomcatUser != null) {
-				sesion.setAttribute("dni", users.get(tomcatUser).getUsername());
+				sesion.setAttribute("dni", users.get(tomcatUser).getUsername()); //Añadimos a la sesión en usuario y contraseña
 				sesion.setAttribute("password", users.get(tomcatUser).getPassword());
 				try {
-					
+					/* Creamos una conexión POST a centro educativo /login */
 					URL url = new URL(urlCE+"/login");
 					HttpURLConnection con = (HttpURLConnection) url.openConnection();
-					
-					
 					con.setRequestMethod("POST");
 					con.setDoOutput(true);
 					con.setRequestProperty("content-type", "application/json");
+					
+					//Le añadimos el cuerpo al mensaje mediante un JSON (En este caso se ha realizado el json de forma literal)
 					
 					DataOutputStream wr = new DataOutputStream (
 							con.getOutputStream());
 		            wr.writeBytes("{\"dni\":\""+ sesion.getAttribute("dni") +"\",\"password\":\""+sesion.getAttribute("password")+"\"}");
 		            wr.close();
 		            
-		            List<String> cookies = con.getHeaderFields().get("Set-Cookie"); 
-		            out.println ("<p> cookies: "+cookies+"</p>");
+		            // Obtenemos las cookies que nos ha devuelto el centro educativo
+		            List<String> cookies = con.getHeaderFields().get("Set-Cookie"); 		            
 		            
-		            
+		            /* Leemos el mensaje que hemos obtenido del centro educativo, deberia ser la KEY */
 		            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 					String inputLine;
 					StringBuffer response2 = new StringBuffer();
@@ -101,162 +99,30 @@ public class login extends HttpServlet {
 					
 					in.close();
 					String key = response2.toString();
-					out.println ("<p> JSON Response: "+key+"</p>");
 					
+					/* Añadimos tanto la KEY como la COOKIE a la sesión actual*/
 					sesion.setAttribute("key", key);
 					sesion.setAttribute("cookies", cookies.get(0));
 		            
 		            con.disconnect();
-		            
-					
 				} catch (Exception e) {
-					sesion.invalidate();
 					
-					//response.sendError(500, "Hubo problemas al recuperar la información." + e);
+					/* En  caso de que falle, invalidamos la sesión y redirigimos a la pagina de error correspondiente*/
+					sesion.invalidate();
 					response.sendRedirect(request.getContextPath() + "/error_centro_educativo.html");
 					return;
 				}	
 			} else {
 				response.sendRedirect(request.getContextPath() + "/error_autenticacion_tomcat.html");
 			}
-		
-		} else {
-			out.println ("<p> sesion.getAttribute(\"dni\"): "+sesion.getAttribute("dni")+"</p>");
-			out.println ("<p> sesion.getAttribute(\"key\"): "+sesion.getAttribute("key")+"</p>");
-			out.println ("<p> sesion.getAttribute(\"cookies\"): "+sesion.getAttribute("cookies")+"</p>");
-			out.println ("<p> sesion.getAttribute(\"password\"): "+sesion.getAttribute("password")+"</p>");
 		}
-		//out.println ("<p> sesion.isNew(): "+sesion.isNew()+"</p>");
-		out.println ("<p> request.getRemoteUser(): "+request.getRemoteUser()+"</p>");
-		out.println ("<p> request.isUserInRole(\"mirol\"): "+request.isUserInRole("mirol")+"</p>");
-		out.println ("<p> request.getUserPrincipal(): "+request.getUserPrincipal()+"</p>");
 		
+		/* Comprobamos cual es el rol del usuario autenticado en tomcat y lo redirigimos al servlet correspondiente */
 		if (request.isUserInRole("rolpro")) {
 			//nothing yet
 		} else if (request.isUserInRole("rolalu")) {
 			response.sendRedirect(request.getContextPath() + "/alumno");
 		}
-		
-		
-		
-		
-		/*try {
-			
-			URL url = new URL(urlCE+"/login");
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			
-			
-			con.setRequestMethod("POST");
-			con.setDoOutput(true);
-			con.setRequestProperty("content-type", "application/json");
-			
-			DataOutputStream wr = new DataOutputStream (
-					con.getOutputStream());
-            wr.writeBytes("{\"dni\":\""+ dni +"\",\"password\":\""+password+"\"}");
-            wr.close();
-            List<String> cookies = con.getHeaderFields().get("Set-Cookie"); 
-            out.println ("<p> cookies: "+cookies+"</p>");
-            
-            
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response2 = new StringBuffer();
-
-			while ((inputLine = in.readLine()) != null) {
-				response2.append(inputLine);
-			}
-			
-			in.close();
-			String key = response2.toString();
-			out.println ("<p> JSON Response: "+key+"</p>");
-            
-            con.disconnect();
-            
-            
-			
-			
-			
-			
-		} catch (Exception e) {
-			response.sendError(500, "Hubo problemas al recuperar la información." + e);
-			return;
-		}*/
-
-		/*
-		try {
-			URL url = new URL(urlCE+"/login");
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			
-			con.setRequestMethod("POST");
-			con.setDoOutput(true);
-			con.setRequestProperty("content-type", "application/json");
-			
-			DataOutputStream wr = new DataOutputStream (
-            con.getOutputStream());
-            wr.writeBytes("{\"dni\":\""+ dni +"\",\"password\":\""+password+"\"}");
-            wr.close();
-            
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response2 = new StringBuffer();
-
-			while ((inputLine = in.readLine()) != null) {
-				response2.append(inputLine);
-			}
-			
-			in.close();
-			String key = response2.toString();
-			out.println ("<p> JSON Response: "+key+"</p>");
-            
-            con.disconnect();
-            
-            
-            
-            try {
-            	
-            	URL url_asignaturas = new URL(urlCE+"/asignaturas?key="+key);
-    			HttpURLConnection con2 = (HttpURLConnection) url_asignaturas.openConnection();
-    			
-    			con2.setRequestMethod("GET");
-    			con2.setDoOutput(true);
-    			con2.setDoInput(true);
-    			
-    			if (con2.getResponseCode() == HttpURLConnection.HTTP_OK) { // success
-    				BufferedReader in2 = new BufferedReader(new InputStreamReader(con.getInputStream()));
-    				String inputLine2;
-    				StringBuffer responseGET = new StringBuffer();
-
-    				while ((inputLine2 = in2.readLine()) != null) {
-    					responseGET.append(inputLine);
-    				}
-    				in2.close();
-
-    				// print result
-    				out.println("<p>PARRAFO¿</p>");
-    				out.println ("<p> JSON Response: "+response.toString()+"</p>");
-    			
-    			} else {
-    				System.out.println("GET request did not work.");
-    			}
-    			
-    			
-                con2.disconnect();
-            	
-            	
-            	
-            	
-            } catch(Exception e) {
-    			response.sendError(500, "Hubo problemas al recuperar la información." + e);
-    			return;
-    		}
-            
-
-		} catch(Exception e) {
-			response.sendError(500, "Hubo problemas al recuperar la información." + e);
-			return;
-		}*/
-			
-		out.println("</body></html>");
 	}
 
 }
