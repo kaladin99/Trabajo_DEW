b29edaef068720fe05a2e605cd5732bab08b98a2
Joel Revert <jrevertvila@gmail.com>
2024-06-07 13:25:51 +0200

    Mostrar imagen del alumno base64

--- src/main/webapp/js/evaluar_alumnos.js
+++ src/main/webapp/js/evaluar_alumnos.js
@@ -18,16 +18,53 @@ let disable_buttons = (array, current) => {
 }
 
 let cambiarNota = (nota) => {
+	alumnos[current_alumno]["nota"] = nota
 	$.ajax({
 	    type: "put",
 	    url: "http://dew-jrevvil-2324.dsicv.upv.es:8080/Trabajo-NOL/cambiar_nota",
 	    data: JSON.stringify({dni:alumnos[current_alumno]["dni"],nota:nota,asignatura:asig}),
 	    contentType: "application/json; charset=utf-8",
-	    success: (res) => {
-	         console.log(res)
-	     },
+        success: (res) => {
+	        console.log(res);
+	        showToast('Nota del alumno ' + alumnos[current_alumno]["nombre"] + ' cambiada correctamente.', 'success');
+	    },
+	    error: (jqXHR, textStatus, errorThrown) => {
+	        console.error('Error:', textStatus, errorThrown);
+	        showToast('Ha ocurrido un error al cambiar la nota.', 'danger');
+	    }
 	})
 }
+
+let showToast = (message, type='success') => {
+    const toastContainer = document.getElementById('toast-container');
+
+    const toast = document.createElement('div');
+    toast.className = `toast align-items-center text-white bg-${type} border-0`;
+    toast.role = 'alert';
+    toast.ariaLive = 'assertive';
+    toast.ariaAtomic = 'true';
+
+    toast.innerHTML = `
+        <div class="d-flex">
+            <div class="toast-body">
+                ${message}
+            </div>
+            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+    `;
+
+    toastContainer.appendChild(toast);
+
+    const bsToast = new bootstrap.Toast(toast);
+    bsToast.show();
+
+    setTimeout(() => {
+        bsToast.hide();
+        toast.addEventListener('hidden.bs.toast', () => {
+            toastContainer.removeChild(toast);
+        });
+    }, 5000);
+ }
  
 $(document).ready(function() {
 	console.log("INPUT VALUE",$("#asignatura").val())
@@ -40,6 +77,7 @@ $(document).ready(function() {
 			alumnos = data
 			$('#alumno_nombre').text(data[current_alumno]["nombre"] + " " + data[current_alumno]["apellidos"])
 			$('#nota').val(data[current_alumno]["nota"])
+			$('#foto_alumno_base64').attr("src", "data:image/png;base64, "+data[current_alumno]["img"]);
 			disable_buttons(alumnos,current_alumno)
 		 }
 	})
@@ -50,6 +88,8 @@ $(document).ready(function() {
 		disable_buttons(alumnos,current_alumno)
 		$('#alumno_nombre').text(alumnos[current_alumno]["nombre"] + " " + alumnos[current_alumno]["apellidos"])
 		$('#nota').val(alumnos[current_alumno]["nota"])
+		$('#foto_alumno_base64').attr("src", "data:image/png;base64, "+alumnos[current_alumno]["img"]);
+
 	});
 	
 	$('#btn_right').click(() => {
@@ -58,12 +98,12 @@ $(document).ready(function() {
 		disable_buttons(alumnos,current_alumno)
 		$('#alumno_nombre').text(alumnos[current_alumno]["nombre"] + " " + alumnos[current_alumno]["apellidos"])
 		$('#nota').val(alumnos[current_alumno]["nota"])
+		$('#foto_alumno_base64').attr("src", "data:image/png;base64, "+alumnos[current_alumno]["img"]);
+
 	});
 	
 	$("#nota").on( "change", function() {
-		toastr.success('La nota del alumno se ha actualizado', 'Nota actualizada')
 		cambiarNota($("#nota").val())
-		console.log("CAMBIA INPUT NOTA")
 	  	modificado = true
 	});
 	
